e447ce776532132d2c5fcf135359d9699f13970f
willowmoon <douglaskelley1001@gmail.com>
2020-04-20 11:39:06 -0400

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.status(200).json({
         "status": 200,
-        "msg" : "server is up and running"
+        "msg" : "Test"
     });
     
 });
