3455b18d4d045a9db4c230fc215efad5417d8eab
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-04-24 14:48:30 -0400

    edit

--- routes/game-route.js
+++ routes/game-route.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const gameController = require('../controllers/game-controller');
+const codeWordsController
 
 router.put('/:id', gameController.createNewUser); // or post.
 
 router.delete('/delete/:id', gameController.deleteUserScore);
+
+router.get('/codewords', )
+
 
 module.exports = router;
